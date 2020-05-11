fb040fd9885b6187a0156d2ed3c179df0777c24f
unknown <prasad.kanhaiya@gmail.com>
2020-05-11 17:51:47 -0400

    End of Section 9

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,24 +1,36 @@
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from 'ngx-gallery-9';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { appRoutes } from './routes';
+import { AuthService } from './_services/auth.service';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { MemberListResolver } from './_resolver/member-list.resolver';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { appRoutes } from './routes';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 
+
+export function tokenGetter(){
+   return localStorage.getItem('token');
+}
 @NgModule({
    declarations: [
       AppComponent,
@@ -28,7 +40,9 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
@@ -36,11 +50,22 @@ import { appRoutes } from './routes';
       FormsModule,
       BsDropdownModule.forRoot(),
       BrowserAnimationsModule,
-      RouterModule.forRoot(appRoutes)
+      TabsModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      MemberDetailResolver,
+      MemberListResolver,
+      ErrorInterceptorProvider,
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,28 +1,32 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolver/member-detail.resolver';
+import { MemberListResolver } from './_resolver/member-list.resolver';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
-import { AuthService } from './_services/auth.service';
 import { AuthGuard } from './_guards/auth.guard';
+
+
 export const appRoutes: Routes = [
-    // {path: 'home', component: HomeComponent},
-    // {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    // {path: 'messages', component: MessagesComponent},
-    // {path: 'list', component: ListsComponent},
-    // {path: '**', redirectTo: 'home', pathMatch: 'full'}
     {path: '', component: HomeComponent},
     {
         path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+            {path: 'members', component: MemberListComponent,
+                resolve: {users: MemberListResolver}},
+            {path: 'members/:id', component: MemberDetailComponent,
+                resolve: {user: MemberDetailResolver}},
+            // {path: 'member/edit', component: MemberEditComponent,
+            //     resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
             {path: 'messages', component: MessagesComponent},
-            {path: 'list', component: ListsComponent}
+            // {path: 'lists', component: ListsComponent, resolve: {users: ListsResolver}},
+            // {path: 'admin', component: AdminPanelComponent, data: {roles: ['Admin', 'Moderator']}},
         ]
     },
-    {path: '**', redirectTo: '', pathMatch: 'full'}
-
+    {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
